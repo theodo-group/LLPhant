bccdc15e305943dd4a749f3ace8b9267b18d3fc1
Kevin Raynel <kevinr@theodo.fr>
2023-08-02 12:11:57 +0200

    Switch to L2 distance

---
+++ src/Doctrine/PgVectorL2OperatorDql.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace LLPhant\Doctrine;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\AST\Node;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+/**
+ * CosineDistanceFunction ::=
+ *     "L2_DISTANCE" "(" VectorPrimary "," VectorPrimary ")"
+ */
+final class PgVectorL2OperatorDql extends FunctionNode
+{
+    private Node $vectorOne;
+
+    private Node $vectorTwo;
+
+    public function parse(Parser $parser): void
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->vectorOne = $parser->ArithmeticFactor(); // Fix that, should be vector
+
+        $parser->match(Lexer::T_COMMA);
+
+        $this->vectorTwo = $parser->ArithmeticFactor(); // Fix that, should be vector
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return 'L2_DISTANCE('.
+            $this->vectorOne->dispatch($sqlWalker).', '.
+            $this->vectorTwo->dispatch($sqlWalker).
+        ')';
+    }
+}
