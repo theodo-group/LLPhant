6bb2b24faa8bd6d1c16788d9ce793e88ba80e4fa
Kevin Raynel <kevinr@theodo.fr>
2023-08-02 11:38:12 +0200

    Add COSINE_DISTANCE dql parser

--- src/VectorStores/DoctrineVectorStore.php
+++ src/VectorStores/DoctrineVectorStore.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Exception\NotSupported;
 use Doctrine\ORM\Exception\ORMException;
 use Doctrine\ORM\OptimisticLockException;
+use LLPhant\Doctrine\PgVectorCosineOperatorDql;
 
 final class DoctrineVectorStore
 {
@@ -41,66 +42,23 @@ final class DoctrineVectorStore
      */
     public function similaritySearch(array $embedding, string $entityClassName = EmbeddingEntityBase::class, int $k = 4, array $additionalArguments = []): array
     {
-        // Get the table name from the entity class
-        $classMetadata = $this->entityManager->getClassMetadata($entityClassName);
-        $tableNameSanitized = $this->sanitize_table_name($classMetadata->getTableName());
-
-        $embeddingString = $this->getEmbeddingString($embedding);
-
-        $sql = "SELECT id FROM {$tableNameSanitized}";
-        $whereClauses = [];
-        $params = [
-            'embeddingString' => $embeddingString,
-            'limitCount' => $k,
-        ];
-
-        foreach ($additionalArguments as $key => $value) {
-            if (! is_scalar($value)) {
-                throw new \InvalidArgumentException("Non-scalar value provided for key {$key}");
-            }
-            // Appending parameter name with a prefix to avoid conflicts with existing parameters
-            $paramName = 'where_'.$key;
-            $whereClauses[] = $key.' = :'.$paramName;
-            // Binding the parameter to its value
-            $params[$paramName] = $value;
-        }
-
-        if ($whereClauses !== []) {
-            $sql .= ' WHERE '.implode(' AND ', $whereClauses);
-        }
-        $sql .= ' ORDER BY embedding <=> :embeddingString ASC LIMIT :limitCount';
-        $resultIds = $this->entityManager->getConnection()->executeQuery($sql, $params)->fetchAllAssociative();
-        /** @var string[]|int[] $ids */
-        $ids = array_column($resultIds, 'id');
+        $this->entityManager->getConfiguration()->addCustomStringFunction('COSINE_DISTANCE', PgVectorCosineOperatorDql::class);
 
         $repository = $this->entityManager->getRepository($entityClassName);
-        /** @var EmbeddingEntityBase[] $entities */
-        $entities = $repository->findBy(['id' => $ids]);
+        $qb = $repository
+            ->createQueryBuilder('e')
+            ->orderBy('COSINE_DISTANCE(e.embedding, :embeddingString)', 'ASC')
+            ->setParameter('embeddingString', $this->getEmbeddingString($embedding))
+            ->setMaxResults($k);
 
-        // We need to sort the entities by the order of the ids from the first query
-        $result = [];
-        foreach ($ids as $id) {
-            $entity = $this->getEntityById($entities, $id);
-            if ($entity instanceof EmbeddingEntityBase) {
-                $result[] = $entity;
-            }
+        foreach ($additionalArguments as $key => $value) {
+            $paramName = 'where_'.$key;
+            $qb
+                ->andWhere(sprintf('e.%s = :%s', $key, $paramName))
+                ->setParameter($paramName, $value);
         }
 
-        return $result;
-    }
-
-    /**
-     * @param  EmbeddingEntityBase[]  $entities
-     */
-    private function getEntityById(array $entities, string|int $id): ?EmbeddingEntityBase
-    {
-        foreach ($entities as $entity) {
-            if ($entity->getId() === $id) {
-                return $entity;
-            }
-        }
-
-        return null;
+        return $qb->getQuery()->getResult();
     }
 
     /**
@@ -111,10 +69,5 @@ final class DoctrineVectorStore
     private function getEmbeddingString(array $embedding): string
     {
         return '['.implode(',', $embedding).']';
-    }
-
-    private function sanitize_table_name(string $table_name): ?string
-    {
-        return preg_replace('/[^a-zA-Z0-9_]/', '', $table_name);
     }
 }
