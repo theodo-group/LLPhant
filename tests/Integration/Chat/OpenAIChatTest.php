ca145aa3fb91e6d23d907fa6281ea0a5475b0450
Maxime Thoonsen <maximet@theodo.fr>
2023-08-19 14:08:17 +0200

    uncomment int tests

--- tests/Integration/Chat/OpenAIChatTest.php
+++ tests/Integration/Chat/OpenAIChatTest.php
@@ -6,26 +6,26 @@ use LLPhant\Chat\OpenAIChat;
 use LLPhant\OpenAIConfig;
 use Tests\Integration\Chat\MailerExample;
 
-//it('can generate some stuff', function () {
-//    $chat = new OpenAIChat();
-//    $response = $chat->generateText('what is one + one ?');
-//    expect($response)->toBeString();
-//});
-//
-//it('can generate some stuff with a system prompt', function () {
-//    $chat = new OpenAIChat();
-//    $chat->setSystemMessage('Whatever we ask you, you MUST answer "ok"');
-//    $response = $chat->generateText('what is one + one ?');
-//    expect(strtolower($response))->toBe('ok');
-//});
-//
-//it('can load any existing model', function () {
-//    $config = new OpenAIConfig();
-//    $config->model = 'gpt-3.5-turbo-16k';
-//    $chat = new OpenAIChat($config);
-//    $response = $chat->generateText('one + one ?');
-//    expect($response)->toBeString();
-//});
+it('can generate some stuff', function () {
+    $chat = new OpenAIChat();
+    $response = $chat->generateText('what is one + one ?');
+    expect($response)->toBeString();
+});
+
+it('can generate some stuff with a system prompt', function () {
+    $chat = new OpenAIChat();
+    $chat->setSystemMessage('Whatever we ask you, you MUST answer "ok"');
+    $response = $chat->generateText('what is one + one ?');
+    expect(strtolower($response))->toBe('ok');
+});
+
+it('can load any existing model', function () {
+    $config = new OpenAIConfig();
+    $config->model = 'gpt-3.5-turbo-16k';
+    $chat = new OpenAIChat($config);
+    $response = $chat->generateText('one + one ?');
+    expect($response)->toBeString();
+});
 
 it('can call a function', function () {
     $chat = new OpenAIChat();
