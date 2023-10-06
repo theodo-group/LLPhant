90e8fe35bf893fa6c38e6cd0b2cd8ac16708927f
Thomas Hercule <t.hercule.sin@gmail.com>
2023-10-06 16:34:35 +0200

    RedisVectorStore minor refactor + rename embedded mocks to .json

--- tests/Integration/Embeddings/VectorStores/Redis/RedisVectorStoreTest.php
+++ tests/Integration/Embeddings/VectorStores/Redis/RedisVectorStoreTest.php
@@ -2,17 +2,29 @@
 
 declare(strict_types=1);
 
-use LLPhant\Embeddings\DataReader\FileDataReader;
 use LLPhant\Embeddings\Document;
-use LLPhant\Embeddings\DocumentSplitter\DocumentSplitter;
 use LLPhant\Embeddings\DocumentUtils;
-use LLPhant\Embeddings\EmbeddingFormatter\EmbeddingFormatter;
-use LLPhant\Embeddings\EmbeddingGenerator\OpenAIEmbeddingGenerator;
 use LLPhant\Embeddings\VectorStores\Redis\RedisVectorStore;
 
 it('tests a full embedding flow with Redis', function () {
-    // [$embeddedDocuments, $embeddingQuery] = getDataFromOpenAi();
-    [$embeddedDocuments, $embeddingQuery] = getMockedData();
+    // Get the already embeded france.txt and paris.txt documents
+    $path = __DIR__.'/../EmbeddedMock/francetxt_paristxt.json';
+    $rawFileContent = file_get_contents($path);
+    if (! $rawFileContent) {
+        throw new Exception('File not found');
+    }
+
+    $rawDocuments = json_decode($rawFileContent, true);
+    $embeddedDocuments = DocumentUtils::createDocumentsFromArray($rawDocuments);
+
+    // Get the embedding of "France the country"
+    $path = __DIR__.'/../EmbeddedMock/france_the_country_embedding.json';
+    $rawFileContent = file_get_contents($path);
+    if (! $rawFileContent) {
+        throw new Exception('File not found');
+    }
+    /** @var float[] $embeddingQuery */
+    $embeddingQuery = json_decode($rawFileContent, true);
 
     $redisClient = new Predis\Client([
         'scheme' => 'tcp',
@@ -23,7 +35,7 @@ it('tests a full embedding flow with Redis', function () {
 
     $vectorStore->addDocuments($embeddedDocuments);
 
-    $searchResult1 = $vectorStore->similaritySearch($embeddingQuery, 10, []);
+    $searchResult1 = $vectorStore->similaritySearch($embeddingQuery, 10);
     expect(getFirstWordOfContentFromResult($searchResult1))->toBe('France');
 
     $requestParam = [
@@ -33,48 +45,10 @@ it('tests a full embedding flow with Redis', function () {
     expect(getFirstWordOfContentFromResult($searchResult2))->toBe('Paris');
 });
 
+/**
+ * @param  Document[]  $result
+ */
 function getFirstWordOfContentFromResult(array $result): string
 {
     return explode(' ', $result[0]->content)[0];
-}
-
-/**
- * @return array{0: Document[], 1: float[]}
- */
-function getMockedData(): array
-{
-    $path = __DIR__.'/../EmbeddedMock/francetxt_paristxt.txt';
-    $rawFileContent = file_get_contents($path);
-    $rawDocuments = json_decode($rawFileContent, true);
-
-    $embeddedDocuments = DocumentUtils::createDocumentsFromArray($rawDocuments);
-
-    $path = __DIR__.'/../EmbeddedMock/france_the_country_embedding.txt';
-    $rawFileContent = file_get_contents($path);
-    $embeddingQuery = json_decode($rawFileContent, true);
-
-    return [$embeddedDocuments, $embeddingQuery];
-}
-
-/**
- * @return array{0: Document[], 1: float[]}
- */
-function getDataFromOpenAi(): array
-{
-    $filePath = __DIR__.'/../PlacesTextFiles';
-    $reader = new FileDataReader($filePath, Document::class);
-
-    $documents = $reader->getDocuments();
-    $splittedDocuments = DocumentSplitter::splitDocuments($documents, 200);
-    $formattedDocuments = EmbeddingFormatter::formatEmbeddings($splittedDocuments);
-
-    $embeddingGenerator = new OpenAIEmbeddingGenerator();
-    $embeddedDocuments = $embeddingGenerator->embedDocuments($formattedDocuments);
-
-    $embeddingGenerator = new OpenAIEmbeddingGenerator();
-    $embeddedDocuments = $embeddingGenerator->embedDocuments($formattedDocuments);
-
-    $embeddingQuery = $embeddingGenerator->embedText('France the country');
-
-    return [$embeddedDocuments, $embeddingQuery];
 }
