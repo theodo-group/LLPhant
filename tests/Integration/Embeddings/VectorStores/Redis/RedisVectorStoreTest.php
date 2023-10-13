cd1307d9d71dd0f61346f01be20498411feb4290
Maxime Thoonsen <maximet@theodo.fr>
2023-10-13 23:46:18 +0200

    fix lint

--- tests/Integration/Embeddings/VectorStores/Redis/RedisVectorStoreTest.php
+++ tests/Integration/Embeddings/VectorStores/Redis/RedisVectorStoreTest.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Integration\Embeddings\VectorStores\Redis;
 
-use LLPhant\Embeddings\Document;
 use LLPhant\Embeddings\DocumentUtils;
 use LLPhant\Embeddings\VectorStores\Redis\RedisVectorStore;
 
