e240f834d8f824ec7d1eda799d151924a291918f
Kevin Raynel <kevinr@theodo.fr>
2023-10-26 15:35:09 +0200

    Add custom vector type for doctrine

---
+++ src/Embeddings/VectorStores/Doctrine/VectorType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace LLPhant\Embeddings\VectorStores\Doctrine;
+
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+
+class VectorType extends Type
+{
+    public const VECTOR = 'vector';
+
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        if(!$platform instanceof PostgreSQLPlatform) {
+            throw Exception::notSupported('VECTORs not supported by Platform.');
+        }
+
+        return sprintf('vector(%d)', $fieldDeclaration['length']);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null) {
+            return [];
+        }
+
+        $value = is_resource($value) ? stream_get_contents($value) : $value;
+
+        return explode(',', $value);
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (! is_array($value) || count($value) === 0) {
+            return null;
+        }
+
+        return '[' . implode(',', $value) . ']';
+    }
+
+    public function getName()
+    {
+        return self::VECTOR;
+    }
+}
