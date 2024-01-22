5976c4d6c45e90c5fac8989811695514958f2ab4
Maxime Thoonsen <maximet@theodo.fr>
2024-01-22 17:59:42 +0100

    update lint

--- tests/Unit/Chat/Function/RichExample.php
+++ tests/Unit/Chat/Function/RichExample.php
@@ -9,11 +9,11 @@ class RichExample
     /**
      * This is the description of the example function from the RichExample class.
      *
-     * @param  string  $stringVar This is the description of the stringVar parameter.
-     * @param  int  $intVar This is the description of the intVar parameter.
-     * @param  float  $floatVar This is the description of the floatVar parameter.
-     * @param  bool  $boolVar This is the description of the boolVar parameter.
-     * @param  mixed[]  $arrayVar This is the description of the arrayVar parameter.
+     * @param  string  $stringVar  This is the description of the stringVar parameter.
+     * @param  int  $intVar  This is the description of the intVar parameter.
+     * @param  float  $floatVar  This is the description of the floatVar parameter.
+     * @param  bool  $boolVar  This is the description of the boolVar parameter.
+     * @param  mixed[]  $arrayVar  This is the description of the arrayVar parameter.
      */
     public function example(string $stringVar, int $intVar, float $floatVar, bool $boolVar, array $arrayVar): void
     {
