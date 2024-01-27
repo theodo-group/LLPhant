5edf8c0ce0707709c2584830affd9a67c4db858c
Benoit Foujols <benoit@foujols.com>
2024-01-27 16:00:39 +0100

    Exploitation des nouveaux modeles embedding OPENIA - text-embedding-3-small (length 512 - 1536) - text-embedding-3-large (length 256 - 1024 - 3072)

--- examples/simple-embeddingOpenIA/script.php
+++ examples/simple-embeddingOpenIA/script.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../../vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
 use LLPhant\Chat\FunctionInfo\FunctionBuilder;
 use LLPhant\Embeddings\DataReader\FileDataReader;
