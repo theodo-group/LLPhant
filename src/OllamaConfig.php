50b3f4c94aa4414df4cfaae8482c984def8ccdbb
Arnaud <nsslive@hotmail.com>
2024-04-01 21:51:46 +0200

    Allow using custom model options
    Hi,
    
    This PR to allow the use of custom values for model options, like those
    one below :
    
    - temperature
    - max_tokens
    - presence_penalty
    - frequency_penalty
    - n
    - logprobs
    - top_logprobs
    - stop
    - user
    - top_p
    - response_format
    
    I used a simple assoc array, instead of defining each options in the
    OpenAIConfig object for 2 reasons:
    
    - 1 simplicity
    - 2 versatility. Since OpenAIConfig object is shared with both
    OpenAIChat and MistralAIChat, and even if the options should be the
    same, we can't know for sure they won't diverge. It allow us to simply
    put some keys that will be transfered to the LLM API.
    
    What do you think ?

--- src/OllamaConfig.php
+++ src/OllamaConfig.php
@@ -13,4 +13,17 @@ class OllamaConfig
     public bool $stream = false;
 
     public bool $formatJson = false;
+
+    /**
+     * model options, example:
+     * - options
+     * - template
+     * - raw
+     * - keep_alive
+     *
+     * @see https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-completion
+     *
+     * @var array<string, mixed>
+     */
+    public array $modelOptions = [];
 }
--- src/OpenAIConfig.php
+++ src/OpenAIConfig.php
@@ -13,4 +13,24 @@ class OpenAIConfig
     public ?Client $client = null;
 
     public string $model;
+
+    /**
+     * model options, example:
+     * - temperature
+     * - max_tokens
+     * - presence_penalty
+     * - frequency_penalty
+     * - n
+     * - logprobs
+     * - top_logprobs
+     * - stop
+     * - user
+     * - top_p
+     * - response_format
+     *
+     * @see https://platform.openai.com/docs/api-reference/chat/create
+     *
+     * @var array<string, mixed>
+     */
+    public array $modelOptions = [];
 }
