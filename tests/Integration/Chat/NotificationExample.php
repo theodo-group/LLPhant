2602709bdd31c383c9b358c4fd703acd478d840c
Maxime Thoonsen <maximet@theodo.fr>
2024-04-09 23:01:56 -1000

    fix void function

---
+++ tests/Integration/Chat/NotificationExample.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration\Chat;
+
+class NotificationExample
+{
+    /**
+     * Send Confirmation to the Slack
+     */
+    public function sendNotificationToSlack(): void
+    {
+        echo 'Sending Notification....';
+    }
+}
