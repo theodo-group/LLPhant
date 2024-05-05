faa977b4d9f4107a40fbcd9c5a51b67fbd4a54aa
salko gjekic <sami42411@gmail.com>
2024-05-05 13:06:53 +0200

    Fixed code styling

--- src/Chat/TokenUsage.php
+++ src/Chat/TokenUsage.php
@@ -5,26 +5,25 @@ namespace LLPhant\Chat;
 
 use OpenAI\Responses\Chat\CreateResponse;
 
-class TokenUsage {
-
-    /* Only logs latest response, not the total amount of all
-    respones */
+class TokenUsage
+{
+    /* Only logs the latest response, not the total amount of all responses */
     public ?string $Prompt_Tokens = '';
     
     public ?string $Completion_Tokens = '';
     
     public ?string $Total_Tokens = '';
 
-
-    public function logLastUsage(CreateResponse $answer) : void {
+    public function logLastUsage(CreateResponse $answer): void
+    {
         $this->Prompt_Tokens = $answer->usage->promptTokens;
         $this->Completion_Tokens = $answer->usage->completionTokens;
         $this->Total_Tokens = $answer->usage->totalTokens;
     }
 
-    public function logLastUsageToArray(CreateResponse $answer) : array {
+    public function logLastUsageToArray(CreateResponse $answer): array
+    {
        $lastUsage = $answer->usage->toArray();
        return $lastUsage;
     }
-
 }
