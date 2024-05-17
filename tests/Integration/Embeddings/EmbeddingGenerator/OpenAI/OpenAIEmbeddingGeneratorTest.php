0155051e767bfd2b250cd466a605db4a2ecca67f
bernard-ng <ngandubernard@gmail.com>
2024-05-17 23:50:28 +0200

    test(#111): open ai embeddings tests

--- tests/Integration/Embeddings/EmbeddingGenerator/OpenAI/OpenAIEmbeddingGeneratorTest.php
+++ tests/Integration/Embeddings/EmbeddingGenerator/OpenAI/OpenAIEmbeddingGeneratorTest.php
@@ -5,9 +5,35 @@ declare(strict_types=1);
 namespace Tests\Integration\Embeddings\EmbeddingGenerator;
 
 use LLPhant\Embeddings\EmbeddingGenerator\OpenAI\OpenAIADA002EmbeddingGenerator;
+use LLPhant\Embeddings\EmbeddingGenerator\OpenAI\OpenAI3LargeEmbeddingGenerator;
+use LLPhant\Embeddings\EmbeddingGenerator\OpenAI\OpenAI3SmallEmbeddingGenerator;
 
 it('can embed some stuff', function () {
-    $llm = new OpenAIADA002EmbeddingGenerator();
-    $embedding = $llm->embedText('I love food');
-    expect($embedding[0])->toBeFloat();
+    $llms = [
+        new OpenAIADA002EmbeddingGenerator(),
+        new OpenAI3LargeEmbeddingGenerator(),
+        new OpenAI3SmallEmbeddingGenerator()
+    ];
+
+    foreach ($llms as $llm) {
+        $embedding = $llm->embedText('I love food');
+        expect($embedding[0])->toBeFloat();
+    }
 });
+
+it('can embed with custom dimensions', function () {
+    $llms = [
+        new OpenAI3LargeEmbeddingGenerator(),
+        new OpenAI3SmallEmbeddingGenerator()
+    ];
+
+    foreach ($llms as $llm) {
+        $embedding = $llm->embedText('I love food', 512);
+        expect(count($embedding))->toBe(512);
+    }
+});
+
+it('throws an exception when trying to set dimensions on a model that does not support it', function () {
+    $llm = new OpenAIADA002EmbeddingGenerator();
+    $llm->embedText('I love food', 512);
+})->fails("Setting embeddings dimensions is only supported in text-embedding-3 and later models.");
