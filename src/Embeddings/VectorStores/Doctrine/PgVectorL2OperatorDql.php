0273c995f88f9fc07ae309fc74c73225fedf90dd
Jason Martoux <apps+git@martoux.com>
2024-05-19 19:57:25 +0200

    fix deprecated constant Lexer for TokenType constant in doctrine/orm 2.19

--- src/Embeddings/VectorStores/Doctrine/PgVectorL2OperatorDql.php
+++ src/Embeddings/VectorStores/Doctrine/PgVectorL2OperatorDql.php
@@ -4,9 +4,9 @@ namespace LLPhant\Embeddings\VectorStores\Doctrine;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\AST\Node;
-use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
 
 /**
  * L2DistanceFunction ::=
@@ -20,16 +20,16 @@ final class PgVectorL2OperatorDql extends FunctionNode
 
     public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
 
         $this->vectorOne = $parser->ArithmeticFactor(); // Fix that, should be vector
 
-        $parser->match(Lexer::T_COMMA);
+        $parser->match(TokenType::T_COMMA);
 
         $this->vectorTwo = $parser->ArithmeticFactor(); // Fix that, should be vector
 
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 
     public function getSql(SqlWalker $sqlWalker): string
