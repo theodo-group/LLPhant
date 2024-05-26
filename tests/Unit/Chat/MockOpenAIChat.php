a70182fea3f36a213d02d20e94428185fec880f7
Franco Lombardo <f.lombardo69@gmail.com>
2024-05-27 00:31:05 +0200

    Fix test

--- tests/Unit/Chat/MockOpenAIChat.php
+++ tests/Unit/Chat/MockOpenAIChat.php
@@ -6,13 +6,16 @@ use OpenAI\Contracts\Resources\ChatContract;
 use OpenAI\Responses\Chat\CreateResponse;
 use OpenAI\Responses\Meta\MetaInformation;
 use OpenAI\Responses\StreamResponse;
+use OpenAI\Testing\Responses\Concerns\Fakeable;
 use OpenAI\Testing\Responses\Fixtures\Chat\CreateResponseFixture;
 
 class MockOpenAIChat implements ChatContract
 {
+    use Fakeable;
+
     public function create(array $parameters): CreateResponse
     {
-        return CreateResponse::from(CreateResponseFixture::ATTRIBUTES, MetaInformation::from([]));
+        return CreateResponse::from(CreateResponseFixture::ATTRIBUTES, self::fakeResponseMetaInformation());
     }
 
     public function createStreamed(array $parameters): StreamResponse
