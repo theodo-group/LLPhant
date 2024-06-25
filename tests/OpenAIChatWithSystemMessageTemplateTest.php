d0fdba1d4fe5047c97a4002cc1943af84a63f68b
cirolaferrara <ciro.laferrara@gmail.com>
2024-06-25 22:05:34 +0200

    Update OpenAIChatWithSystemMessageTemplateTest.php

--- tests/OpenAIChatWithSystemMessageTemplateTest.php
+++ tests/OpenAIChatWithSystemMessageTemplateTest.php
@@ -4,34 +4,31 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use LLPhant\Chat\ChatInterface;
 use LLPhant\Chat\OpenAIChat;
 use LLPhant\Embeddings\EmbeddingGenerator\OpenAI\OpenAI3SmallEmbeddingGenerator;
 use LLPhant\Embeddings\VectorStores\FileSystem\FileSystemVectorStore;
 use LLPhant\OpenAIConfig;
 use LLPhant\Query\SemanticSearch\QuestionAnswering;
 
+function chat(): ChatInterface
+{
+    $config = new OpenAIConfig();
+
+    return new OpenAIChat($config);
+}
+
 it('can generate some stuff', function () {
     $filesVectorStore = new FileSystemVectorStore();
     $embeddingGenerator = new OpenAI3SmallEmbeddingGenerator();
-    $chat = new OpenAIChat();
-  
-    //if ($filesVectorStore->getNumberOfDocuments() === 0) {
-      //$dataReader = new FileDataReader(__DIR__.'/text.txt');
-      //$documents = $dataReader->getDocuments();
-      //$splittedDocuments = DocumentSplitter::splitDocuments($documents, 2000);
-      //$formattedDocuments = EmbeddingFormatter::formatEmbeddings($splittedDocuments);
-      //$embeddedDocuments = $embeddingGenerator->embedDocuments($formattedDocuments);
-      //$filesVectorStore->addDocuments($embeddedDocuments);
-    //}
-
     $qa = new QuestionAnswering(
-      $filesVectorStore,
-      $embeddingGenerator,
-      $chat
+        $filesVectorStore,
+        $embeddingGenerator,
+        chat()
     );
-  
+
     $qa->systemMessageTemplate = 'your name is Ciro. \\n\\n{context}.';
 
-    $response = $chat->generateText('what is your name ?');
-    expect(str_contains($response, 'Ciro'))->toBeTrue();
+    $response = $qa->answerQuestion('what is your name ?');
+    expect($response)->toContain('Ciro');
 });
