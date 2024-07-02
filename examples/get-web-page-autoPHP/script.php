7530e8842186bef464be8e5342718588e644fe67
Franco Lombardo <f.lombardo69@gmail.com>
2024-07-02 22:18:42 +0200

    Update examples at July 2024

--- examples/get-web-page-autoPHP/script.php
+++ examples/get-web-page-autoPHP/script.php
@@ -8,7 +8,9 @@ require_once 'vendor/autoload.php';
 
 $webPageTextGetter = new WebPageTextGetter(true);
 
-$objective = 'Find the name of the french player who scored the most trials in the 2023 rugby world cup, you have to use this page: https://fr.wikipedia.org/wiki/%C3%89quipe_de_France_de_rugby_%C3%A0_XV_%C3%A0_la_Coupe_du_monde_2023 .';
+// This question have already been ingested at the moment
+//$objective = 'Find the name of the French player who scored the most trials in the 2023 rugby world cup, you have to use exactly this page: https://fr.wikipedia.org/wiki/%C3%89quipe_de_France_de_rugby_%C3%A0_XV_%C3%A0_la_Coupe_du_monde_2023 .';
+$objective = 'Find the name of the French top scorer during the Euro 2024 UEFA championship. You can use this page to retrieve fresh data on the topic: https://en.wikipedia.org/wiki/UEFA_Euro_2024';
 
 $function = FunctionBuilder::buildFunctionInfo($webPageTextGetter, 'getWebPageText');
 
