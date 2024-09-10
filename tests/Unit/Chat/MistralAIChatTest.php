d9e30507a4f1be2bc5db1b06de233c1c6438e0a9
Marvin Feldmann <marvin.feldmann@dataquality.de>
2024-09-10 18:40:40 +0200

    Use MistralAIChat in MistralAIChatTest

--- tests/Unit/Chat/MistralAIChatTest.php
+++ tests/Unit/Chat/MistralAIChatTest.php
@@ -4,7 +4,7 @@ namespace Tests\Unit\Chat;
 
 use GuzzleHttp\Psr7\Response;
 use LLPhant\Chat\Message;
-use LLPhant\Chat\OpenAIChat;
+use LLPhant\Chat\MistralAIChat;
 use LLPhant\MistralAIConfig;
 use Mockery;
 use OpenAI\Client;
@@ -13,7 +13,7 @@ use Psr\Http\Message\StreamInterface;
 
 it('no error when construct with no model', function () {
     $config = new MistralAIConfig(apiKey: 'fakeapikey');
-    $chat = new OpenAIChat($config);
+    $chat = new MistralAIChat($config);
     expect(isset($chat))->toBeTrue();
 });
 
@@ -30,7 +30,7 @@ it('returns a stream response using generateStreamOfText()', function () {
 
     $config = new MistralAIConfig();
     $config->client = new Client($transport);
-    $chat = new OpenAIChat($config);
+    $chat = new MistralAIChat($config);
 
     $response = $chat->generateStreamOfText('this is the prompt question');
     expect($response)->toBeInstanceof(StreamInterface::class);
@@ -49,7 +49,7 @@ it('returns a stream response using generateChatStream()', function () {
 
     $config = new MistralAIConfig();
     $config->client = new Client($transport);
-    $chat = new OpenAIChat($config);
+    $chat = new MistralAIChat($config);
 
     $response = $chat->generateChatStream([Message::user('here the question')]);
     expect($response)->toBeInstanceof(StreamInterface::class);
