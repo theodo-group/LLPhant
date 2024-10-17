a738e20f48bbd5ff122f167bedbe96db5b1c3810
Nicolas de Marqué Fromentin <nicolas.demarque@gmail.com>
2024-10-17 21:52:34 +0200

    feat(qdrant): allow default vector as usual in qdrant
    This feature allows the default vector to be used as usual in qdrant,
    ensuring compatibility and consistency with existing functionalities.

--- tests/Integration/Embeddings/VectorStores/Qdrant/QdrantVectorStoreTest.php
+++ tests/Integration/Embeddings/VectorStores/Qdrant/QdrantVectorStoreTest.php
@@ -12,6 +12,7 @@ use LLPhant\Embeddings\EmbeddingGenerator\OpenAI\OpenAIADA002EmbeddingGenerator;
 use LLPhant\Embeddings\VectorStores\Qdrant\QdrantVectorStore;
 use Qdrant\Config;
 use Qdrant\Models\Filter\Condition\MatchString;
+use Qdrant\Models\Request\VectorParams;
 
 it('tests a full embedding flow with Qdrant', function () {
     $filePath = __DIR__.'/../PlacesTextFiles';
@@ -30,6 +31,41 @@ it('tests a full embedding flow with Qdrant', function () {
 
     $collectionName = 'places2';
     $vectorStore = new QdrantVectorStore($config, $collectionName);
+    $vectorStore->createCollectionIfDoesNotExist($collectionName, $embeddingGenerator->getEmbeddingLength());
+    $vectorStore->addDocuments($embededDocuments);
+    $embedding = $embeddingGenerator->embedText('France the country');
+    /** @var Document[] $result */
+    $result = $vectorStore->similaritySearch($embedding, 2);
+
+    // We check that the search return the correct entities in the right order
+    expect(explode(' ', $result[0]->content)[0])->toBe('France');
+
+    $condition = new MatchString('sourceName', 'paris.txt');
+
+    $filter['must'] = [$condition];
+
+    /** @var Document[] $searchResult2 */
+    $searchResult2 = $vectorStore->similaritySearch($embedding, 2, $filter);
+    expect(explode(' ', $searchResult2[0]->content)[0])->toBe('Paris');
+});
+
+it('tests a full embedding flow with Qdrant and euclid distance and null vector name', function () {
+    $filePath = __DIR__.'/../PlacesTextFiles';
+    $reader = new FileDataReader($filePath, Document::class);
+    $documents = $reader->getDocuments();
+    $splittedDocuments = DocumentSplitter::splitDocuments($documents, 100, "\n");
+    $formattedDocuments = EmbeddingFormatter::formatEmbeddings($splittedDocuments);
+
+    $embeddingGenerator = new OpenAIADA002EmbeddingGenerator();
+    $embededDocuments = $embeddingGenerator->embedDocuments($formattedDocuments);
+
+    $host = getenv('QDRANT_HOST');
+    $apiKey = getenv('QDRANT_API_KEY');
+    $config = new Config($host);
+    $config->setApiKey($apiKey);
+
+    $collectionName = 'places3';
+    $vectorStore = new QdrantVectorStore($config, $collectionName, null, VectorParams::DISTANCE_EUCLID);
     $vectorStore->createCollectionIfDoesNotExist($collectionName, $embeddingGenerator->getEmbeddingLength());
     $vectorStore->addDocuments($embededDocuments);
     $embedding = $embeddingGenerator->embedText('France the country');
