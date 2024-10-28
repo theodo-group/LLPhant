a298f80cede58c1382705f22f373251d17ef01fa
Joe Huss <detain@interserver.net>
2024-10-28 01:05:42 +0000

    Added ChainTransformer and ChainDocumentsTransformer classes to provide chained/multiple query/document transformers

---
+++ src/Query/SemanticSearch/ChainTransformer.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LLPhant\Query\SemanticSearch;
+
+class ChainTransformer implements QueryTransformer
+{
+    /** @var QueryTransformer[] */
+    private array $transformers = [];
+
+    public function addTransformer(QueryTransformer $queryTransformer = new IdentityTransformer()): void
+    {
+        $this->transformers[] = $queryTransformer;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function transformQuery(string $query): array
+    {
+        $queries = [$query];
+        foreach ($this->transformers as $transformer) {
+            $newQueries = [];
+            foreach ($queries as $query) {
+                $newQueries = array_merge($newQueries, $transformer->transformQuery($query));
+            }
+            $queries = $newQueries;
+        }
+
+        return $queries;
+    }
+}
