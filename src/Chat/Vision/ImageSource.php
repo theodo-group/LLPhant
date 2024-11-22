f096613c9b06ea97d00a4c7935477a0c886eb24a
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2024-11-22 21:35:33 +0000

    Added images input support for Ollama

--- src/Chat/Vision/ImageSource.php
+++ src/Chat/Vision/ImageSource.php
@@ -2,17 +2,20 @@
 
 namespace LLPhant\Chat\Vision;
 
+use GuzzleHttp\Client;
 use JsonSerializable;
 
 class ImageSource implements JsonSerializable
 {
     private readonly string $url;
+    private ?string $base64 = null;
 
     public function __construct(string $urlOrBase64Image, private readonly ImageQuality $detail = ImageQuality::Auto)
     {
         if ($this->isUrl($urlOrBase64Image)) {
             $this->url = $urlOrBase64Image;
         } elseif ($this->isBase64($urlOrBase64Image)) {
+            $this->base64 = $urlOrBase64Image;
             $this->url = 'data:image/jpeg;base64,'.$urlOrBase64Image;
         } else {
             throw new \InvalidArgumentException('Invalid image URL or base64 format.');
@@ -27,6 +30,17 @@ class ImageSource implements JsonSerializable
     protected function isBase64(string $image): bool
     {
         return \preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $image) === 1;
+    }
+
+    public function getBase64(Client $client): string
+    {
+        if (is_null($this->base64)) {
+            $response = $client->request('GET', $this->url);
+            $imageData = (string)$response->getBody();
+            $this->base64 = base64_encode($imageData);
+        }
+
+        return $this->base64;
     }
 
     /**
