d1460853cde469e1377d0f8e734bc743615e6f68
Thomas Beaujean <thomas.beaujean@dayploy.fr>
2024-12-16 22:45:10 +0100

    Add mistral models

--- src/Chat/Enums/MistralAIChatModel.php
+++ src/Chat/Enums/MistralAIChatModel.php
@@ -2,10 +2,20 @@
 
 namespace LLPhant\Chat\Enums;
 
+/**
+ * see https://docs.mistral.ai/getting-started/models/models_overview/
+ */
 enum MistralAIChatModel: string
 {
     case tiny = 'mistral-tiny';
     case small = 'mistral-small-latest';
     case medium = 'mistral-medium-latest';
     case large = 'mistral-large-latest';
+    case pixtralLarge = 'pixtral-large-latest';
+    case ministral3b = 'ministral-3b-latest';
+    case ministral8b = 'ministral-8b-latest';
+    case codestral = 'codestral-latest';
+    case openMistralNemo = 'open-mistral-nemo';
+    case openCodestralMamba = 'open-codestral-mamba';
+    case mistralModeration = 'mistral-moderation-latest';
 }
